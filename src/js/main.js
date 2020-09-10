ddb80e95d553e4cf27caf0444e4359cfe4f7d8da
Ilya Sovin <levenus8supremus@icloud.com>
2020-09-10 19:46:47 +0300

    исправлены стили

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {
+// Модальные окна
 
-  // Модальные окна
-
-  $('.popup-with-form').magnificPopup({
+$('.popup-with-form').magnificPopup({
     type: 'inline',
     preloader: false,
     focus: '#name',
@@ -10,231 +8,232 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
     callbacks: {
-      beforeOpen: function() {
-        this.st.mainClass = this.st.el.attr('data-effect');
-        if ($(window).width() < 700) {
-          this.st.focus = false;
-        } else {
-          this.st.focus = '#name';
+        beforeOpen: function() {
+            this.st.mainClass = this.st.el.attr('data-effect');
+            if ($(window).width() < 700) {
+                this.st.focus = false;
+            } else {
+                this.st.focus = '#name';
+            }
         }
-      }
     }
-  });
+});
 
+$('.form-modal-window__send-button').on('click', function(e) {
+    e.preventDefault();
+})
 
-  // Мобильное меню, ивенты кнопки
+// Мобильное меню, ивенты кнопки
 
-  const menuToggle = document.querySelector('#menu-toggle');
-  const mobileNavContainer = document.querySelector('#mobile-nav');
-  const buttonToggle = document.querySelector('.header__call-button');
+const menuToggle = document.querySelector('#menu-toggle');
+const mobileNavContainer = document.querySelector('#mobile-nav');
+const buttonToggle = document.querySelector('.header__call-button');
 
-  menuToggle.onclick = function() {
+menuToggle.onclick = function() {
     buttonToggle.classList.toggle('header__call-button_inactive');
     menuToggle.classList.toggle('menu-icon-active');
     mobileNavContainer.classList.toggle('navigation-mobile__list_active');
-  }
+}
 
-  // Зона загрузки файлов
+// Зона загрузки файлов
 
-  Dropzone.autoDiscover = false;
+Dropzone.autoDiscover = false;
 
-  var myDropzone = new Dropzone("li#formUpload", {
+var myDropzone = new Dropzone("li#formUpload", {
     url: "#",
     maxFilesize: 2, // MB
     uploadMultiple: true,
     maxFiles: 5,
     acceptedFiles: '.jpg, .png, jpeg',
     dictDefaultMessage: 'Загрузите изображение',
-  });
+});
 
-  // Шторка до/после
+// Шторка до/после
 
-  $(function() {
+$(function() {
     $(".portfolio__slider").twentytwenty({
-      no_overlay: true, //Do not show the overlay with before and after
+        no_overlay: true, //Do not show the overlay with before and after
     });
     // Mesure your images and divide by 2.
     var imgWidth = $(".portfolio__slider img").width() / 2;
 
     // On the container, apply a left offset of 50% (screen center) - minus half image width.
     $(".portfolio__slider").css({ "position": "relative", "left": "calc(50% - " + imgWidth + "px)" });
-  });
+});
 
-  //  Галерея выполненных работ слик-карусель
+//  Галерея выполненных работ слик-карусель
 
-  $('.js-gallery').slick({
+$('.js-gallery').slick({
     rows: 2,
     slidesPerRow: 4,
     prevArrow: '    <svg class="gallery-arrow mod-prev">\n' +
-      '      <use xlink:href="/assets/img/sprite.svg#angle-left"></use>\n' +
-      '    </svg>',
+        '      <use xlink:href="/assets/img/sprite.svg#angle-left"></use>\n' +
+        '    </svg>',
     nextArrow: '    <svg class="gallery-arrow mod-next">\n' +
-      '      <use xlink:href="/assets/img/sprite.svg#angle-right"></use>\n' +
-      '    </svg>',
+        '      <use xlink:href="/assets/img/sprite.svg#angle-right"></use>\n' +
+        '    </svg>',
     responsive: [{
-        breakpoint: 1025,
-        settings: {
-          slidesPerRow: 3
+            breakpoint: 1025,
+            settings: {
+                slidesPerRow: 3
+            }
+        },
+        {
+            breakpoint: 769,
+            settings: {
+                slidesPerRow: 2
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesPerRow: 1
+            }
         }
-      },
-      {
-        breakpoint: 769,
-        settings: {
-          slidesPerRow: 2
-        }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesPerRow: 1
-        }
-      }
     ]
-  });
+});
 
-  // Слик лайтбокс
+// Слик лайтбокс
 
-  $('.js-gallery').slickLightbox({
+$('.js-gallery').slickLightbox({
     src: 'src',
     itemSelector: '.js-gallery-popup img',
     background: 'rgba(0, 0, 0, .7)'
-  });
+});
 
-  //  Маз слайдер
+//  Маз слайдер
 
-  $('.maz-slider').slick({
+$('.maz-slider').slick({
     slidesToShow: 1,
     centerMode: true,
     dots: true,
 
     prevArrow: '    <svg class="maz-slider-arrow mod-prev">\n' +
-      '      <use xlink:href="/assets/img/sprite.svg#angle-left"></use>\n' +
-      '    </svg>',
+        '      <use xlink:href="/assets/img/sprite.svg#angle-left"></use>\n' +
+        '    </svg>',
     nextArrow: '    <svg class="maz-slider-arrow mod-next">\n' +
-      '      <use xlink:href="/assets/img/sprite.svg#angle-right"></use>\n' +
-      '    </svg>'
-  });
+        '      <use xlink:href="/assets/img/sprite.svg#angle-right"></use>\n' +
+        '    </svg>'
+});
 
-  // Отправка данных с формы
+// Отправка данных с формы
 
-  $('.btn_submit').click(function() {
+$('.btn_submit').click(function() {
 
     const clientPhone = $('.phone-input').val();
     const clientName = $('.name-input').val();
     const textComment = $('.textarea-item').val();
 
     $.ajax({
-      url: "../php/send.php",
-      type: "post",
-      data: {
-        "phone": clientPhone,
-        "name": clientName,
-        "comment": textComment
+        url: "../php/send.php",
+        type: "post",
+        data: {
+            "phone": clientPhone,
+            "name": clientName,
+            "comment": textComment
 
-      },
-      error: function() { $("#erconts").html("Произошла ошибка!"); },
+        },
+        error: function() { $("#erconts").html("Произошла ошибка!"); },
 
-      beforeSend: function() {
-        $("#erconts").html("Отправляем данные...");
-      },
-      success: function(result) {
+        beforeSend: function() {
+            $("#erconts").html("Отправляем данные...");
+        },
+        success: function(result) {
 
-        $('#erconts').html(result);
-        console.log("ntcn");
-      }
+            $('#erconts').html(result);
+            console.log("ntcn");
+        }
     });
-  });
+});
 
-  // Сумматор таблицы прайс-листа
+// Сумматор таблицы прайс-листа
 
-  const service = document.querySelectorAll('.table-row');
-  const finalPrice = document.querySelector('.price-result');
-  const finalTime = document.querySelector('.time-result');
+const service = document.querySelectorAll('.table-row');
+const finalPrice = document.querySelector('.price-result');
+const finalTime = document.querySelector('.time-result');
 
-  for (var i = 0; i < service.length; i++) {
+for (var i = 0; i < service.length; i++) {
 
     service[i].onclick = function() {
 
-      this.classList.toggle('table-row-active');
+        this.classList.toggle('table-row-active');
 
-      let currentCheckbox = this.querySelector('.table-row-checkbox');
+        let currentCheckbox = this.querySelector('.table-row-checkbox');
 
-      currentCheckbox.classList.toggle('table-row-checkbox_active');
+        currentCheckbox.classList.toggle('table-row-checkbox_active');
 
-      calcPrice();
-      calcTime();
+        calcPrice();
+        calcTime();
     }
-  }
+}
 
-  function calcPrice() {
+function calcPrice() {
 
     let price = 0;
 
     for (var i = 0; i < service.length; i++) {
-      if (service[i].classList.contains('table-row-active')) {
-        price += parseInt(service[i].getAttribute('data-price'));
-      }
+        if (service[i].classList.contains('table-row-active')) {
+            price += parseInt(service[i].getAttribute('data-price'));
+        }
     }
 
     finalPrice.innerHTML = price;
-  }
+}
 
-  function calcTime() {
+function calcTime() {
 
     let time = 0;
 
     for (var i = 0; i < service.length; i++) {
-      if (service[i].classList.contains('table-row-active')) {
-        time += parseInt(service[i].getAttribute('data-time'));
-      }
+        if (service[i].classList.contains('table-row-active')) {
+            time += parseInt(service[i].getAttribute('data-time'));
+        }
     }
 
     finalTime.innerHTML = time;
 
-  }
+}
 
-  // Фильтр грузовиков
+// Фильтр грузовиков
 
-  var fActive = '';
-  const tabItems = document.querySelectorAll('.tab-item');
+var fActive = '';
+const tabItems = document.querySelectorAll('.tab-item');
 
-  function filterCard(fabrication) {
+function filterCard(fabrication) {
     if (fActive != fabrication) {
-      $('.painting-card__item').filter('.' + fabrication).fadeIn();
-      $('.painting-card__item').filter(':not(.' + fabrication + ')').fadeOut();
-      fActive = fabrication;
+        $('.painting-card__item').filter('.' + fabrication).fadeIn();
+        $('.painting-card__item').filter(':not(.' + fabrication + ')').fadeOut();
+        fActive = fabrication;
     }
-  }
+}
 
-  $('#russian').click(function() {
+$('#russian').click(function() {
     $(this).addClass('tab-item__active');
     $('.tab-item').not(this).removeClass('tab-item__active');
     filterCard('russia');
-  });
+});
 
-  $('#europian').click(function() {
+$('#europian').click(function() {
     $(this).addClass('tab-item__active');
     $('.tab-item').not(this).removeClass('tab-item__active');
     filterCard('europe');
-  });
+});
 
-  $('#american').click(function() {
+$('#american').click(function() {
     $(this).addClass('tab-item__active');
     $('.tab-item').not(this).removeClass('tab-item__active');
     filterCard('usa');
-  });
+});
 
-  $('#chineese').click(function() {
+$('#chineese').click(function() {
     $(this).addClass('tab-item__active');
     $('.tab-item').not(this).removeClass('tab-item__active');
     filterCard('china');
-  });
+});
 
-  $('#all').click(function() {
+$('#all').click(function() {
     $(this).addClass('tab-item__active');
     $('.tab-item').not(this).removeClass('tab-item__active');
     $('.painting-card__item').fadeIn();
     fActive = 'all';
-  });
-
 });
